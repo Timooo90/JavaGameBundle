f251520b545833941d2f8afb26b68c9227896ac3
Timo Rintamäki <timo.j.rintamaki@utu.fi>
2024-03-22 14:05:58 +0200

    GUIController now receives the ID of squares clicked with left mouse button

--- src/main/java/timooo90/tictactoe/GUIController.java
+++ src/main/java/timooo90/tictactoe/GUIController.java
@@ -1,4 +1,19 @@
 package timooo90.tictactoe;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+
 public class GUIController {
+
+    @FXML
+    private void squareClicked(MouseEvent event) {
+        Object clicked = event.getSource();
+
+        if (clicked.getClass().equals(Rectangle.class)) {
+            Rectangle rectangle = (Rectangle)clicked;
+
+            System.out.println(rectangle.getId());
+        }
+    }
 }
